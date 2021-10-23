c9fe2d5fc108b4d9f7e0b0ca561767dee467db41
Cryptopragmatic <tristan.alberio@gmail.com>
2021-10-23 22:03:18 +0800

    near basics

--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,18 @@ be permanently stored on the blockchain AKA turning data into serials to store i
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 
 use near_sdk::{env, near_bindgen};
+use near_sdk::collections::UnorderedMap; // import lib in order to use UnorderedMap type
+
 
 near_sdk::setup_alloc!();
+//make the data serializable using the following decorators
+\#\[near_bindgen\]
 
+\#\[derive(BorshDeserialize, BorshSerialize)\]
+
+//create NFT data structure using struct
 pub struct NftOwners {
 
-    owners: UnorderedMap;
+    owners: UnorderedMap; //UnorderMap is a dictionary recognized by Near blockchain
   
 }
