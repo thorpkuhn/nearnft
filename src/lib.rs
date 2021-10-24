ceaf9b543d9e4601ea1a5df60a5c86d51355c547
Cryptopragmatic <tristan.alberio@gmail.com>
2021-10-24 18:58:45 +0800

    near basic contract

--- src/lib.rs
+++ src/lib.rs
@@ -19,3 +19,39 @@ pub struct NftOwners {
     owners: UnorderedMap; //UnorderMap is a dictionary recognized by Near blockchain
   
 }
+
+//create a default for nftowner struct. Defaults are required for structs in Rust
+impl Default for NftOwners {
+
+    fn default() -> Self {
+  
+      Self { 
+          //create a new UnorderedMap we also need to provide an identifier ”o” which will identify this map on the storage space.
+  
+          owners: UnorderedMap::new(b“o”.to_vec())
+  
+      }
+  
+    }
+  
+} 
+
+\#\[near_bindgen\]
+
+impl NftOwners { //implement functionality to nftowners
+//setOwner creates an owner, mutable since owner can be altered
+  pub fn setOwner(&mut self, tokenId: String, accountId: String){ 
+
+    self.owners.insert(&tokenId, &accountId); 
+
+  }
+
+  //retrieves the owner of the token
+  // return type of getOwner is Option meaning it can be a String or a null
+pub fn getOwner(&self, tokenId: String) -> Option {
+
+    return self.owners.get(&tokenId);
+
+  }
+
+}
